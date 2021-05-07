d48a2306afcbf12bf900906c73066eb4ebc2c5d4
fcventura02 <filipecvr4@gmail.com>
2021-05-07 18:43:02 -0300

    update route navbar

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function NavBar() {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <img src="/ds_dark.svg" alt="DevSuperior" width="120" />
+                    <Link href="/">
+                        <a className="navbar-brand">
+                            <img src="/ds_dark.svg" alt="DevSuperior" width="120" />
+                        </a>
+                    </Link>
                 </nav>
             </div>
         </div>
