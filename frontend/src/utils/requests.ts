65292bbcdfeeed7bffce249bc0c3cad4fc9c3932
fcventura02 <filipecvr4@gmail.com>
2021-05-07 22:40:25 -0300

    update Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export const api = axios.create({
-    baseURL: process.env.API_URL ?? 'http://localhost:8080/'
+    baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8080/'
 })
