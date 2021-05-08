d2ba59e07eb184311be9dac93066d65c76d220da
fcventura02 <filipecvr4@gmail.com>
2021-05-07 22:07:41 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,26 +1,57 @@
+import { useEffect, useState } from "react"
+import { SalePage } from "../../types/sale"
+import { api } from "../../utils/requests";
+import { formatLocalDate } from '../../utils/format'
+import Pagination from "../Pagination";
+
 export default function DataTable() {
+    const [activePage, setActivePage] = useState(0);
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0,
+    });
+
+    useEffect(() => {
+        api.get(`sales?page=${activePage}&sort=date,desc`)
+            .then(res => {
+                setPage(res.data);
+            })
+    }, [activePage])
+
+    function changePage(index: number){
+        setActivePage(index)
+    }
+
     return (
-        <div className="table-responsive">
-            <table className="table table-striped table-sm">
-                <thead>
-                    <tr>
-                        <th>Data</th>
-                        <th>Vendedor</th>
-                        <th>Clientes visitados</th>
-                        <th>Negócios fechados</th>
-                        <th>Valor</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <>
+            <Pagination page={page} onPageChange={changePage}/>
+            <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Vendedor</th>
+                            <th>Clientes visitados</th>
+                            <th>Negócios fechados</th>
+                            <th>Valor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {page.content && page.content.map(item =>
+                            <tr key={item.id}>
+                                <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                                <td>{item.seller.name}</td>
+                                <td>{item.visited}</td>
+                                <td>{item.deals}</td>
+                                <td>{item.amount}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
